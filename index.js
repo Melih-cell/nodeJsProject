2b2e216ff2de7acb426352ecf37e1335aa88a5ff
Melih Dokuzlar <melihdokuzlar71@gmail.com>
2022-07-12 18:04:46 +0300

    Ödev tamamlandı

---
+++ index.js
@@ -0,0 +1,29 @@
+const User = [
+    {
+        name: 'Melih', surname: 'Dokuzlar'
+    },
+    {
+        name: 'Kamil', surname: 'Koç'
+    },
+    {
+        name: 'Hasan', surname: 'Kezbanoğlu'
+    },
+    {
+        name: 'Selçuk', surname: 'İnan'
+    }
+]
+
+const newWrite = () => {
+    User.map((list) => {
+        console.log(`isim: ${list.name} && soyadı:  ${list.surname}`)
+    })
+}
+
+const newAddList = (newUser, callBack) => {
+    User.push(newUser)
+    callBack();
+}
+
+
+
+newAddList({name: 'Fahri', surname: 'Keser'}, newWrite)
